83a71b0290a25498d4bfc46e2179e16f2c200d14
saharama <saharama@hawaii.edu>
2020-09-22 00:43:47 -1000

    install semantic ui

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header, Menu, Dropdown, Image } from 'semantic-ui-react';
+
+class TopMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless className="topMenu">
+            <Menu.Item>Engineering Network</Menu.Item>
+            <Menu.Item>Home</Menu.Item>
+            <Menu.Item>About</Menu.Item>
+            <Menu.Item>Events</Menu.Item>
+            <Menu.Item>Officers</Menu.Item>
+            <Menu.Item>Members</Menu.Item>
+            <Menu.Item>Photos</Menu.Item>
+            <Menu.Item>Connect with Us</Menu.Item>
+            <Menu.Item>Matthew's EE160</Menu.Item>
+        </Menu>
+    )
+  }
+}
+
+class ENetworkLogo extends React.Component {
+  render() {
+    return (
+        <Container>
+          <Header as="h1" textAlign='centered'> Engineering Network at University of Hawaii at Manoa</Header>
+          <Image centered src="http://www.enuhm.com/wp-content/uploads/2018/10/0925_ENetwork_KC_logo_01-1024x1024.jpg" size='huge' />
+        </Container>
+    )
+  }
+}
+
+class MiddleMessage extends React.Component {
+  render() {
+    return (
+        <Header as="h1">MiddleMenu</Header>
+    )
+  }
+}
+
+class FullWidthImage extends React.Component {
+  render() {
+    return (
+        <Header as="h1">FullWidthImage</Header>
+    )
+  }
+}
+
+class FooterMenu extends React.Component {
+  render() {
+    return (
+        <Header as="h1">FooterMenu</Header>
+    )
+  }
+}
 
 class ENetwork extends React.Component {
 
   render() {
     return (
-        <h1>Engineering Network!</h1>
+        <div>
+          <TopMenu/>
+          <ENetworkLogo/>
+          <MiddleMessage/>
+          <FullWidthImage/>
+          <FooterMenu/>
+        </div>
     );
   }
 }
